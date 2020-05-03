d6df621df9cae18d725d99c11c476c565cb4a058
Douglas de León <douglasdeleon@Douglass-MacBook-Pro.local>
2020-05-02 19:11:53 -0600

    petOwners endpoints and sagas added

--- ejemplos_django/example/owners/views.py
+++ ejemplos_django/example/owners/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+import math
 
-# Create your views here.
+from guardian.shortcuts import assign_perm
+from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
+from permissions.services import APIPermissionClassFactory
+from owners.models import Owner
+from owners.serializers import OwnerSerializer
+
+
+class OwnerViewSet(viewsets.ModelViewSet):
+    queryset = Owner.objects.all()
+    serializer_class = OwnerSerializer
+    permission_classes = (
+        APIPermissionClassFactory(
+            name='OwnerPermission',
+            permission_configuration={
+                'base': {
+                    'create': True,
+                    'list': True,
+                    # 'bulk_happy_birthday': lambda user, req: user.is_authenticated,
+                },
+                'instance': {
+                    'retrieve': True,
+                    'destroy': True,
+                    'update': True,
+                    # 'update_permissions': 'users.add_permissions'
+                }
+            }
+        ),
+    )
