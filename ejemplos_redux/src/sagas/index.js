6bed2deb0e7278a43c3e5eaf188095f0b32c7d67
Douglas de León <douglasdeleon@Douglass-MacBook-Pro.local>
2020-05-02 19:15:12 -0600

    added petOwners sagas to sagas index

--- ejemplos_redux/src/sagas/index.js
+++ ejemplos_redux/src/sagas/index.js
@@ -2,13 +2,17 @@ import { fork, all } from 'redux-saga/effects';
 
 import { watchLoginStarted } from './auth';
 import { watchSayHappyBirthday } from './happyBirthday';
+import { watchFetchPetOwnersStarted, watchAddPetOwnersStarted, watchRemovePetOwnersStarted } from './petOwners';
 
 
 function* mainSaga() {
-  yield all([
-    fork(watchLoginStarted),
-    fork(watchSayHappyBirthday),
-  ]);
+    yield all([
+        fork(watchLoginStarted),
+        fork(watchSayHappyBirthday),
+        fork(watchFetchPetOwnersStarted),
+        fork(watchAddPetOwnersStarted),
+        fork(watchRemovePetOwnersStarted),
+    ]);
 }
 
 
